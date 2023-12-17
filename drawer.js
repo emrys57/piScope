3a253f490005ff1097f1a84203ceefdb995fb03f
emrys57 <e.williams@linkz-im.com>
2023-12-17 14:47:20 +0000

    Try to plot errors as well after line fitting.

--- drawer.js
+++ drawer.js
@@ -1,11 +1,12 @@
 // Purpose: This file contains the code for the drawer.
 // Separated out to a js file so eslint checks it.
 
-import {SmoothieChart, TimeSeries} from './node_modules/smoothie/smoothie.js';
+import {SmoothieChart, TimeSeries} from 'smoothie';
+import {Fitter} from './fitter.js';
 const canvas = document.getElementById('myChart');
 const smoothie = new SmoothieChart({
   millisPerPixel: 400,
-  minValue: 0,
+  minValue: -2000, // to allow for error channels, where an error of 0 displays as -1000
   maxValue: 3500,
   grid: {fillStyle: '#f5f7e9', strokeStyle: 'rgba(150,150,150,0.3)', millisPerLine: 60000, verticalSections: 6, lineWidth: 1},
   tooltipLine: {strokeStyle: '#bbbbbb'},
@@ -13,12 +14,24 @@ const smoothie = new SmoothieChart({
 });
 let loadBundle = true; // When we first start we want to load all the
 // data from the server to get the chart up to date.
+const errorOffset = -1000;
+const errorScale = 100;
+
+const addError = ({channel, time, error}) => {
+  errorLines[channel].append(time, error * errorScale + errorOffset);
+};
+
+const fitters = []; // the straigth line fitters for each channel.
 const lines = [];
+const errorLines = [];
 const numberOfLines = 2;
 const colors = ['rgba(0, 0, 255, 1)', 'rgba(255, 0, 0, 1)'];
 for (let i = 0; i < numberOfLines; i++) {
-  lines.push(new TimeSeries());
+  lines[i] = new TimeSeries();
+  errorLines[i] = new TimeSeries();
+  fitters[i] = new Fitter({channel: i, addError});
   smoothie.addTimeSeries(lines[i], {strokeStyle: colors[i], /*fillStyle: 'rgba(0, 255, 0, 0.6)',*/ lineWidth: 1});
+  smoothie.addTimeSeries(errorLines[i], {strokeStyle: colors[i], /*fillStyle: 'rgba(0, 255, 0, 0.6)',*/ lineWidth: 1});
 }
 smoothie.streamTo(canvas, 1000);
 
@@ -33,7 +46,7 @@ const connectWebSocket = () => {
     const data = JSON.parse(event.data);
     if (Array.isArray(data) && loadBundle) {
       // This is a bundle of data to get the chart up to date.
-      // We don't have data, so we use this bundle to get the chart up to date.
+      // We haven't plotted data yet, so we use this bundle to get the chart up to date.
       data.forEach((d) => {
         addReading(d);
       });
@@ -50,13 +63,12 @@ const connectWebSocket = () => {
     // If the socket drops - it happens - we just reopen it.
     if (event.wasClean) {
       console.log(`Closed cleanly, code=${event.code}, reason=${event.reason}`);
-      connectWebSocket();
     } else {
       // Connection was not closed cleanly, maybe server process crashed or network issues.
       // We can try to reconnect here.
       console.log('WebSocket connection closed unexpectedly. Attempting to reconnect...');
-      setTimeout(connectWebSocket, 5000); // Try to reconnect every 5 seconds.
     }
+    setTimeout(connectWebSocket, 5000); // Try to reconnect after 5 seconds.
   };
 
   const addReading = (data, print = false) => {
@@ -70,6 +82,7 @@ const connectWebSocket = () => {
       return;
     }
     lines[channel].append(time, Number(reading));
+    fitters[channel].fitLineIfTrigger(data);
     if (print) {
       console.log(`channel:${channel} time: ${time}, reading: ${reading}`);
     }
---
+++ fitter.js
@@ -0,0 +1,80 @@
+// Fit a line to the measured points.
+// This waits for the measured value to pass some threshold, then fits all the recent points
+// between two other threholds.
+// Each channel is treated independently.
+// This fits both +ve and -ve slopes.
+
+import regression from 'regression';
+
+class Fitter {
+  constructor(options) {
+    // Destructuring assignment with default values for the options parameter.
+    const {channel = 0, triggerHigh = 3000, triggerLow = 300, limitHigh = 2800, limitLow = 500, lookingForPositiveSlope = true, addError} = options;
+
+    // Assign each option to a property in this class.
+    this.channel = channel;
+    this.triggerHigh = triggerHigh;
+    this.triggerLow = triggerLow;
+    this.limitHigh = limitHigh;
+    this.limitLow = limitLow;
+    this.lookingForPositiveSlope = lookingForPositiveSlope;
+    this.data = [];
+    this.addError = addError;
+  }
+
+  fitLineIfTrigger(datum) {
+    const {time, reading} = datum; // Removed comment of destructuring time
+
+    // Check if the reading is within the limits to be added to data.
+    if (reading <= this.limitHigh && reading >= this.limitLow) {
+      this.data.push([time, reading]);
+    }
+
+    // Determine whether to trigger the line fitting process.
+    let trigger = false;
+    if (this.lookingForPositiveSlope && reading > this.triggerHigh) {
+      trigger = true;
+    } else if (!this.lookingForPositiveSlope && reading < this.triggerLow) {
+      trigger = true;
+    }
+
+    // If a trigger condition is met, fit the line and reset for next fitting cycle.
+    if (trigger) {
+      this.fit(this.data);
+      this.data = [];
+      this.lookingForPositiveSlope = !this.lookingForPositiveSlope;
+      return true;
+    }
+    return false;
+  }
+
+  // Fit a line to the provided data array.
+  fit(data) {
+    // Check if there is enough data to fit a line.
+    if (data.length < 2) {
+      throw new Error('Not enough data to fit a line');
+    }
+
+    // Use the linear regression function from the regression library.
+    const result = regression.linear(data);
+    const [a, b] = result.equation;
+
+    // Calculate and report the error for each data point.
+    data.forEach(([time, reading]) => {
+      const expectedReading = a * time + b;
+      const error = reading - expectedReading;
+      if (this.addError) {
+        this.addError({channel: this.channel, time, error});
+      } else {
+        // Handle the case where addError is not provided or is not a function.
+        throw new Error('addError function is not defined or is not a function');
+      }
+    });
+
+    // Return the result of the regression.
+    return result;
+  }
+}
+
+// Export the Fitter class for use in other modules.
+export {Fitter};
